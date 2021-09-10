86c1d42f819826c141676676a38e8c150be11c6d
nurveda <utoglunurr@gmail.com>
2021-09-10 22:50:13 +0300

    added more hello features

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello there");
         System.out.println("developing hello feature");
+        System.out.println("Local repo is already published on GitHub");
+        System.out.println("Doing more work");
     }
 }
